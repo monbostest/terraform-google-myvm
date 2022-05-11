d0c6c501c401ab9020120cd3b24776839af94bd9
monbostest <52292370+monbostest@users.noreply.github.com>
2022-05-12 02:41:57 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+   default = "news"
+}
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "machine_type" {
+  default = "e2-medium"
+  description = "This is for machine type"
+}
