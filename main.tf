e86672c0a699570ce61c1c6ce7f275c1810e098f
monbostest <52292370+monbostest@users.noreply.github.com>
2022-05-12 21:54:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,39 @@
+resource "google_compute_instance" "vm01" {
+ 
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+   
 
+  
+  allow_stopping_for_update = true
+ 
+  
+  
+ 
+  
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+ 
+  network_interface {
+   network = "default"
+    
+
+    access_config {
+     
+    }
+  }
+
+  metadata = {
+    foo = "news"
+    value = "paper"
+  }
+
+  
+
+}
